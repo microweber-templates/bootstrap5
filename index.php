8ebb16ec5faaad6f46953856457e55902f3a2f4a
Nikolay <rachkuw@gmail.com>
2022-04-14 15:45:09 +0300

    update

--- index.php
+++ index.php
@@ -13,10 +13,7 @@
 <?php include template_dir() . "header.php"; ?>
 
 <div class="edit main-content" data-layout-container rel="content" field="content">
-    <module type="layouts" template="content/skin-1"/>
-    <module type="layouts" template="content/skin-2"/>
-    <module type="layouts" template="content/skin-3"/>
-    <module type="layouts" template="content/skin-4"/>
+
 </div>
 
 <?php include template_dir() . "footer.php"; ?>
