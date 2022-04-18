75dc9b8eff91c23372c9164fa1b15543d566047d
Nikolay <rachkuw@gmail.com>
2022-04-18 13:33:37 +0300

    update

--- partials/header/parts/multilanguage.php
+++
@@ -1,7 +0,0 @@
-<?php if ($multilanguage == 'yes'): ?>
-    <li class="nav-item dropdown ps-3  my-xl-0 my-1">
-        <div>
-            <module type="multilanguage" />
-        </div>
-    </li>
-<?php endif; ?>
--- partials/header/parts/profile_link.php
+++ partials/header/parts/profile_link.php
@@ -1,4 +1,4 @@
-<?php if ($profile_link == 'yes'): ?>
+
     <script>
         var $window = $(window), $document = $(document);
         $document.ready(function () {
@@ -34,4 +34,4 @@
             <?php endif; ?>
         </ul>
     </li>
-<?php endif; ?>
+
